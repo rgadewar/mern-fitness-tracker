9451aa896042f49c9764a34e8193b0c166077081
Rupa Gadewar <rupa@gadewar.com>
2023-10-24 13:37:45 -0500

    Delete functinality

--- client/src/Reducers/ActivityReducer.js
+++ client/src/Reducers/ActivityReducer.js
@@ -1,74 +1,86 @@
 // Import your action types here (make sure they match your actual action types)
 import {
-    FETCH_ACTIVITIES,
-    ADD_ACTIVITY,
-    UPDATE_ACTIVITY,
-    DELETE_ACTIVITY,
-    SET_WEEK_GOAL,
-    UPDATE_WEEKLY_PROGRESS,
-    SET_TABLE_DATA
-  } from './actions';
-  
-  // Define your initial state
-  const initialState = {
-    activities: [], // Initialize with an empty array
-    weekGoal: 100,
-    initialWeeklyProgress: 0,
-    weeklyProgress: 0,
-    tableData: [],
+  FETCH_ACTIVITIES,
+  ADD_ACTIVITY,
+  UPDATE_ACTIVITY,
+  DELETE_ACTIVITY,
+  SET_WEEK_GOAL,
+  UPDATE_WEEKLY_PROGRESS,
+  SET_TABLE_DATA,
+} from "./actions";
 
-  };
-  
-  // Create your reducer function (rename it to activityReducer)
-  const activityReducer = (state = initialState, action) => {
-    switch (action.type) {
-      case FETCH_ACTIVITIES:
+// Define your initial state
+const initialState = {
+  activities: [], // Initialize with an empty array
+  weekGoal: 100,
+  initialWeeklyProgress: 0,
+  weeklyProgress: 0,
+  tableData: [],
+};
+
+// Create your reducer function (rename it to activityReducer)
+const activityReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_ACTIVITIES:
+      return {
+        ...state,
+        activities: action.payload, // Update the activities array in the state
+      };
+
+    case ADD_ACTIVITY:
+      const activityName = action.payload.name;
+      const existingActivity = state.activities.find(
+        (activity) => activity.name === activityName
+      );
+
+      if (existingActivity) {
+        // Activity with the same name already exists, update its goal
+        existingActivity.goal = action.payload.goal;
         return {
           ...state,
-          activities: action.payload, // Update the activities array in the state
+          activities: [...state.activities],
         };
-  
-      case ADD_ACTIVITY:
+      } else {
+        // Activity with a unique name, add it to the activities array
         return {
           ...state,
-          activities: [...state.activities, action.payload], // Add a new activity to the activities array
+          activities: [...state.activities, action.payload],
         };
-  
-      case UPDATE_ACTIVITY:
-        const updatedActivities = state.activities.map((activity) =>
-          activity.id === action.payload.id ? action.payload : activity
-        );
-        return {
-          ...state,
-          activities: updatedActivities, // Update an existing activity in the activities array
-        };
-  
-      case DELETE_ACTIVITY:
-        const filteredActivities = state.activities.filter(
-          (activity) => activity.id !== action.payload
-        );
-        return {
-          ...state,
-          activities: filteredActivities, // Remove an activity from the activities array
-        };
+      }
 
-      case SET_WEEK_GOAL:
-        return { ...state, weekGoal: action.payload };
+    case UPDATE_ACTIVITY:
+      const updatedActivities = state.activities.map((activity) =>
+        activity.id === action.payload.id ? action.payload : activity
+      );
+      return {
+        ...state,
+        activities: updatedActivities, // Update an existing activity in the activities array
+      };
 
-        case UPDATE_WEEKLY_PROGRESS:
-          return {
-            ...state,
-            weeklyProgress: state.weeklyProgress + action.payload,
-          };
-        case "RESET":
+    case DELETE_ACTIVITY:
+      const filteredActivities = state.activities.filter(
+        (activity) => activity.id !== action.payload
+      );
+      return {
+        ...state,
+        activities: filteredActivities, // Remove an activity from the activities array
+      };
 
-          return initialState; 
- 
-      default:
-        return state; // Return the unchanged state for unrecognized actions
-    }
-  };
-  
-  // Export the activityReducer function
-  export default activityReducer;
-  
+    case SET_WEEK_GOAL:
+      return { ...state, weekGoal: action.payload };
+
+    case UPDATE_WEEKLY_PROGRESS:
+      return {
+        ...state,
+        weeklyProgress: state.weeklyProgress + action.payload,
+      };
+    case "RESET":
+      return initialState;
+
+    default:
+      return state; // Return the unchanged state for unrecognized actions
+  }
+};
+
+// Export the activityReducer function
+export default activityReducer;
--- client/src/components/UserActivities/index.jsx
+++ client/src/components/UserActivities/index.jsx
@@ -4,13 +4,14 @@ import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper
 import { GET_USER_ACTIVITIES } from '../../utils/queries';
 import AuthService from '../../utils/auth';
 import { DELETE_ACTIVITY } from '../../utils/mutations';
-import { useDispatch, useSelector } from 'react-redux'; // Import both useDispatch and useSelector from react-redux
-import { fetchActivities } from '../../Reducers/actions'; 
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchActivities } from '../../Reducers/actions';
 
 const UserActivities = () => {
   const [userProfile, setUserProfile] = useState(null);
-  const [userActivities, setUserActivities] = useState([]); 
   const dispatch = useDispatch();
+  const userActivities = useSelector((state) => state.activities);
+  const [updatedActivity, setUpdatedActivity] = useState(null); // State to track updated activity
 
   const { loading, data, refetch } = useQuery(GET_USER_ACTIVITIES, {
     variables: { userId: userProfile ? userProfile.data._id : null },
@@ -18,9 +19,10 @@ const UserActivities = () => {
 
   useEffect(() => {
     if (data && data.getUserActivities) {
-      dispatch(fetchActivities(data.getUserActivities.activities)); 
+      dispatch(fetchActivities(data.getUserActivities.activities));
     }
   }, [data, dispatch]);
+
   useEffect(() => {
     if (AuthService.loggedIn()) {
       const userProfile = AuthService.getProfile();
@@ -34,44 +36,39 @@ const UserActivities = () => {
     }
   }, [userProfile, refetch]);
 
-  console.log("userProfile", userProfile);
-
   useEffect(() => {
-    // Update userActivities when data changes
-    if (data?.getUserActivities?.activities) {
-      setUserActivities(data.getUserActivities.activities);
-    }
-  }, [data]);
+    refetch(); // Trigger a refetch when 'userActivities' change
+  }, [userActivities]);
 
   const [deleteActivityMutation] = useMutation(DELETE_ACTIVITY);
   const handleDeleteActivity = async (activityId) => {
     try {
       const response = await deleteActivityMutation({ variables: { activityId } });
-  
+
       if (response.data && response.data.deleteActivity) {
-        // Assuming the deletion was successful, update the userActivities state
-        const updatedUserActivities = userActivities.filter(
-          (activity) => activity._id !== activityId
-        );
-        setUserActivities(updatedUserActivities);
+        const updatedUserActivities = userActivities.filter((activity) => activity._id !== activityId);
+        dispatch(fetchActivities(updatedUserActivities)); // Dispatch the updated activities to the store
+
+        // Set the updated activity in the state
+        const deletedActivity = userActivities.find((activity) => activity._id === activityId);
+        setUpdatedActivity(deletedActivity);
+
+        // You can also reset the message after a timeout
+        setTimeout(() => setUpdatedActivity(null), 5000);
       } else {
-        // Handle other success cases or provide feedback
         console.log("Deletion was not successful");
       }
     } catch (error) {
-      // Handle errors and provide feedback to the user
       console.error("Error deleting activity:", error);
     }
   };
-  
 
   if (loading) return <p>Loading...</p>;
-
-  
 
   return (
     <div>
       <h2>User Activities</h2>
+      {updatedActivity && <p>Updated activity: {updatedActivity.name}</p>} {/* Render the updated activity message */}
       <TableContainer component={Paper}>
         <Table>
           <TableHead>
@@ -83,7 +80,7 @@ const UserActivities = () => {
           </TableHead>
           <TableBody>
             {userActivities.map((activity) => (
-              <TableRow key={activity._id}>
+              <TableRow key={activity.name}>
                 <TableCell>{activity.name}</TableCell>
                 <TableCell>{activity.goal}</TableCell>
                 <TableCell>
